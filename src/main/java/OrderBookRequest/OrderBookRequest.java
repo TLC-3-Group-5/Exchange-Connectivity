2c949c11b0d239fd51294bebd5866e18e00584e7
BaldeMountaga <baldemtaga@gmail.com>
2021-03-16 18:21:12 +0000

    created some functionalities

---
+++ src/main/java/OrderBookRequest/OrderBookRequest.java
@@ -0,0 +1,40 @@
+package OrderBookRequest;
+
+public class OrderBookRequest {
+
+    private String id;
+    private String product;
+    private String side;
+
+    public OrderBookRequest(){}
+
+    public OrderBookRequest(String id, String product, String side) {
+        this.id = id;
+        this.product = product;
+        this.side = side;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getProduct() {
+        return product;
+    }
+
+    public void setProduct(String product) {
+        this.product = product;
+    }
+
+    public String getSide() {
+        return side;
+    }
+
+    public void setSide(String side) {
+        this.side = side;
+    }
+}
