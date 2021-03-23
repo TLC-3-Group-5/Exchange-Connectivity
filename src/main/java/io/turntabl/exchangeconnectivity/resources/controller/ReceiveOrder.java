4b700d6e2ae78a22d8ccd6a47580cd70e0917a79
Smart <kwesi.smart@turntabl.io>
2021-03-23 22:19:10 +0000

    Fix errors

--- src/main/java/io/turntabl/exchangeconnectivity/resources/controller/ReceiveOrder.java
+++ src/main/java/io/turntabl/exchangeconnectivity/resources/controller/ReceiveOrder.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import io.turntabl.exchangeconnectivity.resources.model.Response;
 import io.turntabl.exchangeconnectivity.resources.model.Trade;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +16,7 @@ import redis.clients.jedis.Jedis;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 // Class to receive order
 @Component
@@ -23,26 +25,41 @@ public class ReceiveOrder {
     @Autowired
     RestTemplate restTemplate;
 
-    public ReceiveOrder(){
+    @Autowired
+    Environment env;
 
-    }
+    public ReceiveOrder() {}
+
     ObjectMapper objectMapper = new ObjectMapper();
     // Method to keep checking queue
     public void receiveOrder() throws JsonProcessingException {
-        Jedis jedis = new Jedis("localhost", 6379);
-        while(true)
-        {
+        Jedis jedis = new Jedis(
+            Optional.ofNullable(env.getProperty("app.SPRING_REDIS_URL")).orElse(""),
+            Optional.ofNullable(Integer.parseInt(env.getProperty("app.SPRING_REDIS_PORT"))).orElse(0)
+        );
+
+        while(true) {
             List<String> output = jedis.blpop(0, "exchange1");
             Trade request = objectMapper.readValue(output.get(1), Trade.class);
             System.out.println(output.get(1));
             System.out.println(request);
             ResponseEntity<String> exchangeId =
-                    restTemplate.postForEntity("http://localhost:8084/create-order", request, String.class);
+                    restTemplate.postForEntity(
+                        Optional.ofNullable(env.getProperty("app.host_url")).orElse("")
+                            .concat("/create-order"),
+                        request, 
+                        String.class
+                    );
             System.out.println(exchangeId.getBody());
             Map<String, String> variables = new HashMap<>();
             variables.put("tradeId", String.valueOf(request.getId()));
             variables.put("exchangeId",exchangeId.getBody().replaceAll("\"", ""));
-            restTemplate.put("http://localhost:8083/trade/{tradeId}/{exchangeId}", Trade.class , variables);
+            restTemplate.put(
+                Optional.ofNullable(env.getProperty("app.trade_engine_url")).orElse("")
+                    .concat("/trade/{tradeId}/{exchangeId}"),
+                Trade.class,
+                variables
+            );
         }
     }
 }
